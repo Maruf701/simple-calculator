a965b803e931b087a92737b67bf963f49519b3c7
Maruf Khan <marufatrai12@gmail.com>
2024-08-20 20:47:52 +0600

    JS Calculator

--- javascript/script.js
+++ javascript/script.js
@@ -2,9 +2,31 @@
 
 
 
-    var x = 10;
-    var y = 5;
+    var firstNumber = parseFloat(prompt("Enter a First Number : "));
+    var lastNumber = parseFloat(prompt("Enter a Last Number : "));
 
-    var result = x + y;
+    var result;
 
-    document.write(result);
+    result = (firstNumber + lastNumber);
+
+    document.write(firstNumber + " + " + lastNumber + " = " + result + "<br>");
+    var result;
+
+    result = (firstNumber - lastNumber);
+
+    document.write(firstNumber + " - " + lastNumber + " = " + result + "<br>");
+    var result;
+
+    result = (firstNumber * lastNumber);
+
+    document.write(firstNumber + " x " + lastNumber + " = " + result + "<br>");
+    var result;
+
+    result = (firstNumber / lastNumber);
+
+    document.write(firstNumber + " / " + lastNumber + " = " + result + "<br>");
+    var result;
+
+    result = (firstNumber % lastNumber);
+
+    document.write(firstNumber + " % " + lastNumber + " = " + result + "<br>");
