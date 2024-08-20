ff1dfea8a406d8c8d36a34beb393190c848d5331
Maruf Khan <marufatrai12@gmail.com>
2024-08-20 20:13:58 +0600

    JS

--- javascript/script.js
+++ javascript/script.js
@@ -2,31 +2,9 @@
 
 
 
-    var firstNumber = prompt("Enter a First Number : ");
-    var secondNumber = prompt("Enter a Second Number : ");
+    var Celsius = parseInt(prompt("Enter a Celsius : "));
 
-    var cv1 = parseFloat(firstNumber);
-    var cv2 = parseFloat(secondNumber);
-
-    var result;
-
-    result = cv1 + cv2;
-
-    document.write(cv1 + " + " + cv2 + " = " + result + "<br>");
+    var fahrenheit =(Celsius * 9/5) + 32;
 
 
-    result = cv1 - cv2;
-
-    document.write(cv1 + " - " + cv2 + " = " + result + "<br>");
-
-    result = cv1 * cv2;
-
-    document.write(cv1 + " x " + cv2 + " = " + result + "<br>");
-
-    result = cv1 / cv2;
-
-    document.write(cv1 + " / " + cv2 + " = " + result + "<br>");
-
-    result = cv1 % cv2;
-
-    document.write(cv1 + " % " + cv2 + " = " + result + "<br>");
+    document.write(fahrenheit);
