e1cd38da572466ae0cee0a6f00661e3e97f6f200
Maruf Khan <marufatrai12@gmail.com>
2024-08-20 19:19:32 +0600

    Js

---
+++ javascript/script.js
@@ -0,0 +1,32 @@
+
+
+
+
+    var firstNumber = prompt("Enter a First Number : ");
+    var secondNumber = prompt("Enter a Second Number : ");
+
+    var cv1 = parseFloat(firstNumber);
+    var cv2 = parseFloat(secondNumber);
+
+    var result;
+
+    result = cv1 + cv2;
+
+    document.write(cv1 + " + " + cv2 + " = " + result + "<br>");
+
+
+    result = cv1 - cv2;
+
+    document.write(cv1 + " - " + cv2 + " = " + result + "<br>");
+
+    result = cv1 * cv2;
+
+    document.write(cv1 + " x " + cv2 + " = " + result + "<br>");
+
+    result = cv1 / cv2;
+
+    document.write(cv1 + " / " + cv2 + " = " + result + "<br>");
+
+    result = cv1 % cv2;
+
+    document.write(cv1 + " % " + cv2 + " = " + result + "<br>");
