ac1aa75ab9a48b128a5cf8ef59209ead5df1cd91
Maruf Khan <123176550+Maruf701@users.noreply.github.com>
2024-08-24 20:24:59 +0600

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,66 +1,18 @@
+var num1 = parseFloat(prompt("Enter a First Number : "));
+var num2 = parseFloat(prompt("Enter a Second Number : "));
 
+var result;
+result = num1 + num2;
+document.write(num1 + " + " + num2 + " = " + result + <br>);
 
+result = num1 - num2;
+document.write(num1 + " - " + num2 + " = " + result + <br>);
 
+result = num1 * num2;
+document.write(num1 + " x " + num2 + " = " + result + <br>);
 
-    document.write(25 > 20); // true
+result = num1 / num2;
+document.write(num1 + " / " + num2 + " = " + result + <br>);
 
-    document.write("<br>");
-
-    document.write(25 < 20); // False
-
-    document.write("<br>");
-
-    document.write(25 >= 25 ); // true
-
-    document.write("<br>");
-
-    document.write(25 <= 25 ); // true
-
-
-    document.write("<br>");
-
-    document.write(25 === "25" ); // False value of type Check 3 Equel
-
-    
-
-
-    document.write("<br>");
-
-    document.write(25 != 25 ); // False 
-
-    
-
-
-    document.write("<br>");
-    document.write("<br>");
-    document.write("<br>");
-
-
-    var x = 25;
-    var y = 20;
-    var z = 40;
-
-
-    document.write(x>y && x>z); // False
-
-    
-    document.write("<br>");
-    document.write("<br>");
-
-
-    var num1 = 30;
-    var num2 = 50;
-    var num3 = 15;
-
-
-    document.write(num1>num3 || num1>num2); // true
-
-    
-
-    
-    
-    document.write("<br>");
-    document.write("<br>");
-
-
-    document.write(!(5==4));
+result = num1 % num2;
+document.write(num1 + " % " + num2 + " = " + result + <br>);
